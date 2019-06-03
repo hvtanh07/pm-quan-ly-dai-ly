a4faa10ecb5c82701be694047b63e74504aace4c
Excubizzy <l3unkachou@gmail.com>
2019-06-03 20:14:05 +0700

    Revert "Update CreateQLDL.sql"
    This reverts commit a89d917792e9aa8c56e5d00ace9dcfc30dd9cdc0.

--- Database/CreateQLDL.sql
+++ Database/CreateQLDL.sql
@@ -28,99 +28,35 @@ CREATE TABLE [dbo].[tblLoaiDaiLy]
 	[maxno] int NOT NULL
 )
 CREATE TABLE [dbo].[tblMatHang]
-<<<<<<< HEAD
-(
-	[maMatHang] int NOT NULL PRIMARY KEY,
-	[tenmathang] nvarchar(50) NOT NULL,
-=======
 (	
 	[tenmathang] nvarchar(50) NOT NULL PRIMARY KEY,		
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
 	[hanSuDung] datetime2(7) NOT NULL,
 	[gia]		int NOT NULL,
 	[donViTinh] nvarchar(50)	
 )
-<<<<<<< HEAD
-CREATE TABLE [dbo].[tblDaiLy]
-(
-	[maDaiLy]	int NOT NULL PRIMARY KEY,
-	[tenDaiLy]	nvarchar(50) NOT NULL,
-=======
 CREATE TABLE [dbo].[tblHoSoDaiLy]
 (	
 	[tenDaiLy]	nvarchar(50) NOT NULL PRIMARY KEY,
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
 	[diachi]	nvarchar(50) NOT NULL,
 	[email]		nvarchar(30),
 	[loaiDaiLy]	int	NOT NULL,
 	[ngayTiepNhan]	datetime2(7) NOT NULL,
 	[quan]		nvarchar(20) NOT NULL,
-<<<<<<< HEAD
-	[dienthoai]	int NOT NULL,
-	[nohientai] int
-=======
 	[dienthoai]	nvarchar(20) NOT NULL,
 	[nohientai] int NOT NULL,
 	FOREIGN KEY (loaiDaiLy) REFERENCES tblLoaiDaiLy(loaiDaiLy),
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
 )
 CREATE TABLE [dbo].[tblPhieuThuTien]
 (
 	[maPhieu]	int	NOT NULL PRIMARY KEY,
-<<<<<<< HEAD
-	[maDaiLy]	int NOT NULL,
-	[ngayThuTien] datetime2(7) NOT NULL,
-	[soTienThu] int NOT NULL,
-=======
 	[tenDaiLy]	nvarchar(50) NOT NULL,	
 	[ngayThuTien] datetime NOT NULL,
 	[soTienThu] int NOT NULL
 	FOREIGN KEY (tenDaiLy) REFERENCES tblHoSoDaiLy(tenDaiLy),
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
 )
 CREATE TABLE [dbo].[tblPhieuXuatHang]
 (
 	[maXuatHang]	int NOT NULL PRIMARY KEY,
-<<<<<<< HEAD
-	[maDaiLy]		int NOT NULL,
-	[ngayLapPhieu]	datetime2(7) NOT NULL
-)
-CREATE TABLE [dbo].[tblChiTietPhieuXH]
-(
-	[maXuatHang] int NOT NULL PRIMARY KEY,
-	[maMatHang] int NOT NULL,
-	[soluong]	int NOT NULL,
-	[donViTinh]	nvarchar(50) NOT NULL,
-	[dongia] int NOT NULL,
-	[thanhtien] int NOT NULL
-)
-CREATE TABLE [dbo].[tblLoaiDaiLy]
-(
-	[maLoaiDaiLy] int NOT NULL PRIMARY KEY,
-	[tenLoaiDaiLy]	nvarchar(30) NOT NULL,
-	[notoida]	int NOT NULL
-)
-CREATE TABLE [dbo].[tblNoDaiLy]
-(
-	[maDaiLy]	int NOT NULL PRIMARY KEY,
-	[nodau]		int,
-	[nocuoi]	int,
-	[phatsinh]	int
-)
-CREATE TABLE [dbo].[PhieuBaoCaoCongNo]
-{
-	[maPhieuBaoCao] int NOT NULL PRIMARY KEY,
-	[thang] datetime
-}
-CREATE TABLE [dbo].[tblDoanhThuDaiLy]
-(
-	[maBaoCao]	int NOT NULL PRIMARY KEY,
-	[maDaiLy]	int NOT NULL,
-	[sophieuxuat]	int NOT NULL,
-	[tongtrigia]	int NOT NULL,
-	[tyle]			float NOT NULL,
-	[thang]		datetime NOT NULL
-=======
 	[tenDaiLy]	nvarchar(50) NOT NULL,
 	[ngayLapPhieu]	datetime NOT NULL,
 	[tongtien]		int NOT NULL,
@@ -138,56 +74,9 @@ CREATE TABLE [dbo].[tblPhieubaocaoCongNo]
 (
 	[maPhieu] int NOT NULL PRIMARY KEY,			
 	[thang] int NOT NULL	
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
 )
 CREATE TABLE [dbo].[tblPhieubaocaoDoanhThu]
 (
-<<<<<<< HEAD
-	[MaxLoaiDaiLy] int,
-	[MaxTrongQuan] int,
-	[SoluongMH] int,
-	[SoluongDVT] int
-)
-USE [QLDL]
-GO
-ALTER TABLE [dbo].[tblDaiLy]
-ADD CONSTRAINT FK_LOAIDL FOREIGN KEY (loaiDaiLy) REFERENCES [dbo].[tblLoaiDaiLy](maLoaiDaiLy)
-GO
-ALTER TABLE [dbo].[tblNoDaiLy]
-ADD CONSTRAINT FK_MADL_NDL FOREIGN KEY (maDaiLy) REFERENCES [dbo].[tblDaiLy](maDaiLy)
-GO
-ALTER TABLE [dbo].[tblDoanhThuDaiLy]
-ADD CONSTRAINT FK_MADL_DTDL FOREIGN KEY (maDaiLy) REFERENCES [dbo].[tblDaiLy](maDaiLy)
-GO
-ALTER TABLE [dbo].[tblPhieuThuTien]
-ADD CONSTRAINT FK_MADL_PTT FOREIGN KEY (maDaiLy) REFERENCES [dbo].[tblDaiLy](maDaiLy)
-GO
-ALTER TABLE [dbo].[tblPhieuXuatHang]
-ADD CONSTRAINT FK_MADL_PXH FOREIGN KEY (maDaiLy) REFERENCES [dbo].[tblDaiLy](maDaiLy)
-GO
-ALTER TABLE [dbo].[tblChiTietPhieuXH]
-ADD CONSTRAINT FK_MAXH FOREIGN KEY (maXuatHang) REFERENCES [dbo].[tblPhieuXuatHang](maXuatHang)
-GO
-ALTER TABLE [dbo].[tblChiTietPhieuXH]
-ADD CONSTRAINT FK_MAMH FOREIGN KEY (maMatHang) REFERENCES [dbo].[tblMatHang] (maMatHang)
-
-SET DATEFORMAT DMY 
-GO
-GO
-INSERT INTO [dbo].[tblLoaiDaiLy] VALUES ('1','Loai Dai Ly 1','30000000')
-GO
-INSERT INTO [dbo].[tblLoaiDaiLy] VALUES ('2','Loai Dai Ly 2','40000000')
-GO
-INSERT INTO [dbo].[tblLoaiDaiLy] VALUES ('3','Loai Dai Ly 3','65000000')
-GO
-INSERT INTO [dbo].[tblDaiLy] ([maDaiLy], [tenDaiLy], [diachi],[email], [loaiDaiLy], [dientich],[sonhanvien], [ngayTiepNhan],[quan], [dienthoai],[nohientai]) 
-VALUES ('1','Dai Ly A','Dia Chi Dai Ly A','DLA@email.com','1','120','5','20/03/1999','12','0909111222','10000000')
-GO
-INSERT INTO [dbo].[tblDaiLy] VALUES ('2','Dai Ly B','Dia Chi Dai Ly B','DLB@email.com','2','150','10','30/04/1975','9','0909222333','15000000')
-GO
-INSERT INTO [dbo].[tblDaiLy] VALUES ('3','Dai Ly C','Dia Chi Dai Ly C','DLC@email.com','3','132','9','19/05/1890','9','0930041975','25000000')
-GO
-=======
 	[maPhieu] int NOT NULL PRIMARY KEY,		
 	[tongDoanhThu]	int NOT NULL,	
 	[thang] int NOT NULL	
@@ -270,4 +159,3 @@ VALUES ('dl5', 'dc5', 'dl5@gmail.com', 2, '9/2/2019', N'Quận Thủ Đức', 39
 --ADD CONSTRAINT FK_MAXH FOREIGN KEY (maXuatHang) REFERENCES [dbo].[tblPhieuXuatHang](maXuatHang)
 
 
->>>>>>> 2c1cf5c3b667b69b5ed958f77697579c5513afae
